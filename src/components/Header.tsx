5bee1afda2047cc933f9b8b52e7b3265daf58d66
shimul shil <72187771+shimulshil@users.noreply.github.com>
2023-01-02 00:51:07 +0100

    Logout function update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import {SyntheticEvent } from "react"
-import { useNavigate} from 'react-router-dom';
+import {SyntheticEvent} from "react"
+import {useLocation, useNavigate} from 'react-router-dom';
 import {Navbar,Container,Nav} from 'react-bootstrap';
 
 function Header() {
@@ -15,6 +15,8 @@ function Header() {
     sessionStorage.clear();
     navigate("/login");
   }
+  let location = useLocation();
+    console.log(location.state);
 
   return (
     <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
@@ -22,11 +24,16 @@ function Header() {
         <Navbar.Brand href='/'>React_Typescript Login_Registration_logout project</Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
+          {location.state ? (
+            <Nav className='ms-auto'>
+              <Nav.Link onClick={logout}>Logout</Nav.Link>
+            </Nav>
+          ) : (
             <Nav className='ms-auto'>
               <Nav.Link href='/signup'>Sign Up</Nav.Link>
               <Nav.Link href='/login'>Login</Nav.Link>
-              <Nav.Link onClick={logout}>Logout</Nav.Link>
-              </Nav>
+            </Nav>
+          )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
